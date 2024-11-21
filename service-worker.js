5240e141d78de2d632e19c81ee1c3973fcb2639a
Sanju Santhosh <68913047+TheDAOS@users.noreply.github.com>
2024-11-22 00:27:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 // Cache resources for offline use
 const CACHE_NAME = 'my-pwa-cache-v1';
 const urlsToCache = [
-//  '/',
-//  '/index.html',
-//   '/styles.css',
-//  '/app.js',
-//  '/icons/icon-192x192.png',
-//  '/icons/icon-512x512.png',
-    '/C1P-W8A/',
-    '/C1P-W8A/index.html',
-    '/C1P-W8A/app.js',
-    '/C1P-W8A/icons/icon-192x192.png',
-    '/C1P-W8A/icons/icon-512x512.png',
+  '/',
+  '/index.html',
+//  '/styles.css',
+  '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+//    '/C1P-W8A/',
+//    '/C1P-W8A/index.html',
+//    '/C1P-W8A/app.js',
+//    '/C1P-W8A/icons/icon-192x192.png',
+//    '/C1P-W8A/icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', (event) => {
